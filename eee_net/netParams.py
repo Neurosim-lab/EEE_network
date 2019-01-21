bca2de6a259931ca4cc6a259c0b38f5bf40fa482
graham <joe.w.graham@gmail.com>
2019-01-21 15:58:06 -0700

    Our cell models are imported and functional

--- eee_net/netParams.py
+++ eee_net/netParams.py
@@ -1,5 +1,6 @@
 from netpyne import specs
 import os
+import copy
 
 ## Network parameters
 netParams = specs.NetParams()  # object of class NetParams to store the network parameters
@@ -20,7 +21,7 @@ netParams.popParams['PT5_1'] = {'cellType': 'PT5', 'numCells': int(numPT5cells/4
 netParams.popParams['PT5_2'] = {'cellType': 'PT5', 'numCells': int(numPT5cells/4), 'ynormRange': ynormRange, 'cellModel': 'HH'}
 netParams.popParams['PT5_3'] = {'cellType': 'PT5', 'numCells': int(numPT5cells/4), 'ynormRange': ynormRange, 'cellModel': 'HH'}
 netParams.popParams['PT5_4'] = {'cellType': 'PT5', 'numCells': int(numPT5cells/4), 'ynormRange': ynormRange, 'cellModel': 'HH'}
-netParams.popParams['PV5'] = {'cellType': 'PV', 'numCells': numPV5cells, 'ynormRange': ynormRange, 'cellModel': 'HH'}
+netParams.popParams['PV5'] = {'cellType': 'PV5', 'numCells': numPV5cells, 'ynormRange': ynormRange, 'cellModel': 'HH'}
 
 
 ## Set path to cells directory
@@ -30,17 +31,30 @@ PV_path   = os.path.join(cellpath, 'FS3.hoc')
 
 
 ## Import PV5 cell
-cellRule = netParams.importCellParams(label='PV5', conds={'cellType':'PV'}, fileName=PV_path, cellName='FScell1', cellInstance=True)
-netParams.cellParams['PVrule'] = cellRule
+cellRule = netParams.importCellParams(label='PV5', conds={'cellType':'PV5'}, fileName=PV_path, cellName='FScell1', cellInstance=True)
+netParams.cellParams['PV5'] = cellRule
 
+## Import eeeS cell (PT5)
+cellRule = netParams.importCellParams(label='PT5_1', conds={'pop':'PT5_1'}, fileName=eeeS_path, cellName='MakeCell', cellInstance=True)
+netParams.cellParams['PT5_1'] = cellRule
 
-## Cell property rules
-cellRule = {'conds': {'cellType': 'PT5'},  'secs': {}}  # cell rule dict
-cellRule['secs']['soma'] = {'geom': {}, 'mechs': {}}                              # soma params dict
-cellRule['secs']['soma']['geom'] = {'diam': 15, 'L': 14, 'Ra': 120.0}                   # soma geometry
-cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.13, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}      # soma hh mechanism
-netParams.cellParams['Erule'] = cellRule                          
+# ## Import eeeS cell (PT5)
+# cellRule = netParams.importCellParams(label='PT5_1', conds={'cellType':'PT5_1'}, fileName=eeeS_path, cellName='MakeCell', cellInstance=True)
+# netParams.cellParams['PT5_1'] = cellRule
 
+## Then copy the cellRule for the other pops (faster than importing again)
+## Note: we are creating 1 cell type per pop because they could potentially have different noise and connectivity params           
+for label in ['PT5_2', 'PT5_3', 'PT5_4']:    
+    cellRule = copy.deepcopy(netParams.cellParams['PT5_1'].todict())
+    cellRule['conds']['pop'] = [label]
+    netParams.cellParams[label] = cellRule
+
+# ## Then copy the cellRule for the other pops (faster than importing again)
+# ## Note: we are creating 1 cell type per pop because they could potentially have different noise and connectivity params           
+# for label in ['PT5_2', 'PT5_3', 'PT5_4']:    
+#     cellRule = copy.deepcopy(netParams.cellParams['PT5_1'].todict())
+#     cellRule['conds']['cellType'] = [label]
+#     netParams.cellParams[label] = cellRule
 
 
 ## Synaptic mechanism parameters
@@ -50,7 +64,7 @@ netParams.synMechParams['inh'] = {'mod': 'Exp2Syn', 'tau1': 0.6, 'tau2': 8.5, 'e
 
 # Stimulation parameters
 netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 20, 'noise': 0.3}
-netParams.stimTargetParams['bkg->all'] = {'source': 'bkg', 'conds': {'cellType': ['PV','PT5']}, 'weight': 0.01, 'delay': 'max(1, normal(5,2))', 'synMech': 'exc'}
+netParams.stimTargetParams['bkg->all'] = {'source': 'bkg', 'conds': {'cellType': ['PV5','PT5']}, 'weight': 0.01, 'delay': 'max(1, normal(5,2))', 'synMech': 'exc'}
 
 
 ## Cell connectivity rules
@@ -61,9 +75,10 @@ netParams.connParams['PT5->all'] = {
   'delay': 'dist_3D/propVelocity',      # transmission delay (ms) 
   'synMech': 'exc'}                     # synaptic mechanism 
 
-netParams.connParams['PV->PT5'] = {
-  'preConds': {'cellType': 'PV'}, 'postConds': {'pop': ['PT5']},       #  I -> E
+netParams.connParams['PV5->PT5'] = {
+  'preConds': {'cellType': 'PV5'}, 'postConds': {'cellType': ['PT5']},       #  I -> E
   'probability': '0.4*exp(-dist_3D/probLengthConst)',   # probability of connection
   'weight': 0.001,                                      # synaptic weight 
   'delay': 'dist_3D/propVelocity',                      # transmission delay (ms) 
   'synMech': 'inh'}                                     # synaptic mechanism 
+

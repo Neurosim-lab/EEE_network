cddfca6cf760bcbf6a0df822032aafc5564e7f64
graham <joe.w.graham@gmail.com>
2019-04-02 17:16:21 -0700

    About to run large long sim on Comet

--- eee_net/batch.py
+++ eee_net/batch.py
@@ -18,8 +18,8 @@ def batchTest():
 	b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py')
 	
 	# Set output folder, grid method (all param combinations), and run configuration
-	b.batchLabel = 'batch_test_20190401'
-	b.saveFolder = 'batch_data_20190401'
+	b.batchLabel = 'batch_test_20190402'
+	b.saveFolder = 'batch_data_20190402'
 	b.method = 'grid'
 	b.runCfg = {'type': 'hpc_slurm',
 				'allocation': 'shs100', 
--- eee_net/cfg.py
+++ eee_net/cfg.py
@@ -11,7 +11,7 @@ cfg.duration = 100000
 cfg.dt = 0.025                
 cfg.verbose = False           
 cfg.recordStep = 1             
-cfg.simLabel = 'eee_net_20190401'
+cfg.simLabel = 'eee_net_20190402'
 cfg.saveFolder = 'data'
 cfg.savePickle = False
 cfg.saveJson = True
@@ -24,8 +24,8 @@ cfg.hParams.celsius = 32.0
 cfg.hParams.v_init  = -73.7
 
 # Network variables
-cfg.numPT5cells = 80
-cfg.numPV5cells = 20
+cfg.numPT5cells = 800
+cfg.numPV5cells = 200
 cfg.sizeY       = 1600
 cfg.sizeX       = 400
 cfg.sizeZ       = 300
@@ -97,6 +97,8 @@ cfg.recordTraces['V_dend_0'] = {'sec':'basal_0', 'loc':0.5, 'var':'v'}
 #cfg.recordTraces['V_dend_7'] = {'sec':'basal_7', 'loc':0.5, 'var':'v'}
 cfg.recordTraces['V_dend_8'] = {'sec':'basal_8', 'loc':0.5, 'var':'v'}
 cfg.recordTraces['V_dend_9'] = {'sec':'basal_9', 'loc':0.5, 'var':'v'}
+
+cfg.recordCells = {'include': [('PT5_1', 0), ('PT5_2', 0), ('PT5_3', 0), ('PT5_4', 0), ('PV5', 0)]}
 
 cfg.printPopAvgRates = True
 

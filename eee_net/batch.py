a1181d3b4c70f2a0f142450ff9f3ef67e1643602
graham <joe.w.graham@gmail.com>
2019-04-08 16:02:47 -0700

    Pushing to submit batch to Comet

--- eee_net/batch.py
+++ eee_net/batch.py
@@ -18,12 +18,12 @@ def batchTest():
 	b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py')
 	
 	# Set output folder, grid method (all param combinations), and run configuration
-	b.batchLabel = 'batch_20190405'
-	b.saveFolder = 'batch_data_20190405'
+	b.batchLabel = 'batch_20190408'
+	b.saveFolder = 'batch_data_20190408'
 	b.method = 'grid'
 	b.runCfg = {'type': 'hpc_slurm',
 				'allocation': 'shs100', 
-				'walltime': '1:30:00',
+				'walltime': '2:00:00',
 				'nodes': 1,
 				'coresPerNode': 24,
 				'email': 'joe.w.graham@gmail.com',

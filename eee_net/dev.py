09ea0043e584c6214f30379e06688d4929adb7b9
graham <joe.w.graham@gmail.com>
2019-06-10 18:50:03 -0700

    plot_batch_ind_raster is now functional in dev.py

--- eee_net/dev.py
+++ eee_net/dev.py
@@ -15,7 +15,7 @@ def analyze_batch(batchLabel, batchdatadir=batchdatadir):
 
     batch_analysis.plot_batch_raster(batch, timeRange=[100, 1000], markerSize=0.5, orderInverse=False)
     batch_analysis.plot_vtraces(batch, timerange=[100, 1000])
-    batch_analysis.plot_batch_conn(batch)
+    #batch_analysis.plot_batch_conn(batch)
 
 
 #analyze_batch('v01_batch03')
@@ -26,7 +26,7 @@ def analyze_batch(batchLabel, batchdatadir=batchdatadir):
 
 # Individual plots
 
-def plot_batch_ind_conn(batchLabel, batchdatadir='data', includePre = ['all'], includePost = ['all'], feature = 'strength', orderBy = 'gid', figSize = (10,10), groupBy = 'pop', groupByIntervalPre = None, groupByIntervalPost = None, graphType = 'matrix', synOrConn = 'syn', synMech = None, connsFile = None, tagsFile = None, clim = None, fontSize = 12, saveData = None, saveFig = True, showFig = False, save=True, outputdir="batch_figs", filename=None, **kwargs):
+def plot_batch_ind_conn(batchLabel, batchdatadir='data', includePre = ['all'], includePost = ['all'], feature = 'strength', orderBy = 'gid', figSize = (10,10), groupBy = 'pop', groupByIntervalPre = None, groupByIntervalPost = None, graphType = 'matrix', synOrConn = 'syn', synMech = None, connsFile = None, tagsFile = None, clim = None, fontSize = 12, saveData = None, showFig = False, save=True, outputdir='batch_figs'):
     """Plots individual connectivity plots for each parameter combination."""
 
     from netpyne import specs
@@ -67,13 +67,15 @@ def plot_batch_ind_conn(batchLabel, batchdatadir='data', includePre = ['all'], i
 
         for feature in features:
 
-            if saveFig:
+            if save:
                 saveFig = batchdatadir + '/' + batchLabel + '/' + 'connFig_' + feature + simLabel + '.png'
+            else:
+                saveFig = None
 
             sim.analysis.plotConn(includePre=includePre, includePost=includePost, feature=feature, orderBy=orderBy, figSize=figSize, groupBy=groupBy, groupByIntervalPre=groupByIntervalPre, groupByIntervalPost=groupByIntervalPost, graphType=graphType, synOrConn=synOrConn, synMech=synMech, connsFile=connsFile, tagsFile=tagsFile, clim=clim, fontSize=fontSize, saveData=saveData, saveFig=saveFig, showFig=showFig)
 
 
-batchLabel = 'v01_batch18'
+batchLabel = 'v01_batch20'
 params, data = batch_utils.load_batch(batchLabel, batchdatadir=batchdatadir)
 batchData = (batchLabel, params, data)
 include = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5']
@@ -82,16 +84,93 @@ plot_batch_ind_conn(batchData, includePre=include, includePost=include)
 
 
 
-#batchLabel = "v01_batch03"
-#params, data = batch_utils.load_batch(batchLabel, batchdatadir=batchdatadir)
-#batch = (batchLabel, params, data)
-#include = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5']
-#plot_batch_ind_conn(batch, includePre=include, includePost=include, saveFig=True)
 
-#include = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5']
-#plot_batch_ind_conn("v01_batch15") #, includePre=include, includePost=include, saveFig=True)
-#plot_batch_ind_conn("v01_batch15", includePre=include, includePost=include, saveFig=True)
-#plot_batch_ind_conn("v01_batch16", includePre=include, includePost=include, saveFig=True)
+
+
+
+
+
+
+
+
+
+
+def plot_batch_ind_raster(batchLabel, batchdatadir='data', include=['allCells'], timeRange=None, maxSpikes=1e8, orderBy='gid', orderInverse=False, labels='legend', popRates=False, spikeHist=None, spikeHistBin=5, syncLines=False, figSize=(10,8), saveData=None, showFig=False, save=True, outputdir='batch_figs'):
+    """Plots individual raster plots for each parameter combination."""
+
+    from netpyne import specs
+
+    if type(batchLabel) == str:
+        params, data = batch_utils.load_batch(batchLabel, batchdatadir=batchdatadir)
+    elif type(batchLabel) == tuple:
+        batchLabel, params, data = batchLabel
+    else:
+        raise Exception()
+
+    simLabels = data.keys()
+
+    for simLabel in simLabels:
+
+        print('Plotting sim: ' + simLabel)
+
+        datum = data[simLabel]
+
+        cfg = specs.SimConfig(datum['simConfig'])
+        cfg.createNEURONObj = False
+
+        sim.initialize()  # create network object and set cfg and net params
+        sim.loadAll('', data=datum, instantiate=False)
+        sim.setSimCfg(cfg)
+        try:
+            print('Cells created: ' + str(len(sim.net.allCells)))
+        except:
+            print('Alternate sim loading...')
+            sim.net.createPops()     
+            sim.net.createCells()
+            sim.setupRecording()
+            sim.gatherData() 
+
+        sim.allSimData = datum['simData']
+
+        if save:
+            saveFig = batchdatadir + '/' + batchLabel + '/' + 'rasterFig' + simLabel + '.png'
+        else:
+            saveFig = None
+
+        sim.analysis.plotRaster(include=include, timeRange=timeRange, maxSpikes=maxSpikes, orderBy=orderBy, orderInverse=orderInverse, labels=labels, popRates=popRates, spikeHist=spikeHist, spikeHistBin=spikeHistBin, syncLines=syncLines, figSize=figSize, saveData=saveData, saveFig=saveFig, showFig=showFig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+batchLabel = 'v01_batch20'
+params, data = batch_utils.load_batch(batchLabel, batchdatadir=batchdatadir)
+batchData = (batchLabel, params, data)
+
+include = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5']
+#plot_batch_ind_conn(batchData, includePre=include, includePost=include)
+
+plot_batch_ind_raster(batchData)
+
+
+
+
+
+
+
+
+
+
 
 
 # sim.load(batchdatadir + '/' + batchLabel + '/' + batchLabel + curSim + '.json', instantiate=False)

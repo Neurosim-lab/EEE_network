a686bd8130e740e728c32703e95702ae3eb2f966
graham <joe.w.graham@gmail.com>
2019-08-09 11:56:24 -0600

    Preparing to run v01_batch21 on Comet

--- eee_net/batch.py
+++ eee_net/batch.py
@@ -4,7 +4,7 @@ import os
 
 runType = 'hpc_slurm' # Either 'hpc_slurm' or 'mpi_bulletin'
 #runType = 'mpi_bulletin' # Either 'hpc_slurm' or 'mpi_bulletin'
-batchLabel = 'v01_batch20'
+batchLabel = 'v01_batch21'
 
 def batchRun():
 	# Create variable of type ordered dictionary (NetPyNE's customized version) 

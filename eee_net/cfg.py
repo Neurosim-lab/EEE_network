13546091afdacbf3e6dbb774158f9d1b256dedde
graham <joe.w.graham@gmail.com>
2019-10-11 12:49:09 -0700

    eee_net_50

--- eee_net/cfg.py
+++ eee_net/cfg.py
@@ -15,7 +15,7 @@ cfg.numCells = 10000
 cfg.dt = 0.025                
 cfg.verbose = False           
 cfg.recordStep = 1             
-cfg.simLabel = 'eee_net_49'
+cfg.simLabel = 'eee_net_50'
 
 baseFolder = '/scratch/06322/tg856217'
 #baseFolder = 'data'

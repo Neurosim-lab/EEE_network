d99899f3c4af93ad336e9cdf4b4c5a899759ab96
graham <joe.w.graham@gmail.com>
2019-12-16 17:51:19 -0700

    Added Poirazi model to repo, copied over initial Netpyne files

---
+++ poirazi/init.py
@@ -0,0 +1,60 @@
+"""
+init.py
+
+Usage:
+    python init.py # Run simulation, optionally plot a raster
+
+MPI usage:
+    mpiexec -n 4 nrniv -python -mpi init.py
+
+Contributors: salvadordura@gmail.com
+"""
+
+#import matplotlib; matplotlib.use('Agg')  # to avoid graphics error in servers
+from netpyne import sim
+
+cfg, netParams = sim.readCmdLineArgs()
+
+def initRand(): 
+    for c in sim.net.cells:
+        noise = c.secs['soma']['pointps']['noise']
+        seed1 = noise['seed1']
+        seed2 = noise['seed2']
+        seed3 = noise['seed3']
+        noise['hObj'].noiseFromRandom123(seed1, seed2, seed3)
+
+
+sim.initialize(
+    simConfig = cfg,    
+    netParams = netParams)    # create network object and set cfg and net params
+sim.net.createPops()          # instantiate network populations
+sim.net.createCells()         # instantiate network cells based on defined populations
+sim.net.connectCells()        # create connections between cells based on params
+sim.net.addStims()            # add network stimulation
+
+#initRand()                    # set the seeds for randomization of Gfluctp
+
+sim.setupRecording()          # setup variables to record (spikes, V traces, etc)
+sim.runSim()                  # run parallel Neuron simulation  
+sim.gatherData()              # gather spiking data and cell info from each node
+sim.saveData()                # save params, cell info and sim output to file
+sim.analysis.plotData()       # plot spike raster etc
+
+# Additional analyses
+
+includePre  = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5_1', 'PV5_2']
+includePost = ['PT5_1', 'PT5_2', 'PT5_3', 'PT5_4', 'PV5_1', 'PV5_2']
+feature     = 'strength' #'divergence' #'convergence' #'strength' #'numConns' 
+groupBy     = 'pop' #'cell'
+orderBy     = 'gid' #'y'
+synOrConn   = 'conn' #'syn'
+
+sim.analysis.plotConn(saveFig=True, showFig=False, saveData=True, includePre=includePre, includePost=includePost, feature=feature, groupBy=groupBy, orderBy=orderBy, synOrConn=synOrConn)
+
+# import batch_analysis as ba
+# batchdatadir = '/scratch/06322/tg856217/'
+# batchname = 'v01_batch65'
+# batch = ba.plot_vtraces(batchname, batchdatadir=batchdatadir, outputdir=batchdatadir + batchname)
+
+
+
---
+++ poirazi/mods/NMDA_syn.mod
@@ -0,0 +1,83 @@
+COMMENT
+//****************************//
+// Created by Alon Polsky 	//
+//    apmega@yahoo.com		//
+//		2007			//
+//****************************//
+ENDCOMMENT
+
+TITLE NMDA synapse 
+
+NEURON {
+	POINT_PROCESS nmda_spike
+	USEION ca READ cai WRITE ica VALENCE 2
+	NONSPECIFIC_CURRENT inmda 
+
+	RANGE e ,gmax,inmda
+	RANGE gnmda
+
+	GLOBAL n, gama,tau1,tau2
+
+}
+
+UNITS {
+	(nA) 	= (nanoamp)
+	(mV)	= (millivolt)
+	(nS) 	= (nanomho)
+	(mM)    = (milli/liter)
+        F	= 96480 (coul)
+        R       = 8.314 (volt-coul/degC)
+
+}
+
+PARAMETER {
+	gmax=1	(nS)
+	e= 0.0	(mV)
+	tau1=90	(ms)
+	tau2=5	(ms)	
+	gama=0.08 	(/mV) 
+	dt (ms)
+	v		(mV)
+	n=0.25		(/mM) 
+}
+
+ASSIGNED { 
+	inmda		(nA)   
+	gnmda		(nS)
+	ica 		(mA/cm2)
+	cai		(mM)	
+}
+STATE {
+	A (nS)
+	B (nS)
+}
+
+INITIAL {
+      gnmda=0 
+	A=0
+	B=0
+}
+
+BREAKPOINT {
+	
+	SOLVE state METHOD cnexp
+	
+	gnmda=(A-B)/(1+n*exp(-gama*v))
+	
+	inmda = (1e-3) * gnmda * (v-e)
+	ica = inmda/10
+}
+
+DERIVATIVE state {
+	
+	A'=-A/tau1
+	B'=-B/tau2
+	
+}
+
+NET_RECEIVE (weight) {
+	gmax=weight
+	state_discontinuity( A, A+ gmax)
+	state_discontinuity( B, B+ gmax)
+}
+

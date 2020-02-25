834189794444bedda78763442f1fcb8761032435
graham <joe.w.graham@gmail.com>
2020-02-25 01:06:56 -0500

    Removed loops in experiment.hoc, replaced by single run

--- L5microcircuit/experiment/experiment.py
+++ L5microcircuit/experiment/experiment.py
@@ -49,13 +49,14 @@ for pyr_index, pyr_soma_trace in enumerate(pyr_soma_traces):
     #plt.plot(time, pyr_distap_traces[pyr_index], label='dist ap')
     plt.legend()
 
-
+"""
 for inh_index, inh_trace in enumerate(inh_traces):
     plt.figure()
     plt.plot(time, inh_trace)
     plt.xlabel('Time (ms)')
     plt.ylabel('Membrane Potential (mV)')
     plt.title('Inhibitory cell ' + str(inh_index))
+"""
 
 spike_ind = h.idvec.to_python()
 spike_time = h.timevec.to_python()
@@ -85,6 +86,7 @@ spike_trains = make_spike_trains(spike_times, time_range=[500, 6000])
 
 spike_trains = spike_trains[0:6]
 
+"""
 sync_profile = spk.spike_sync_profile(spike_trains)
 x, y = sync_profile.get_plottable_data()
 plt.figure()
@@ -114,17 +116,8 @@ plt.xlabel('Time (ms)')
 plt.title('Spike Distance Profile (SD = %.4f)' % spike_profile.avrg())
 print("SPIKE distance: %.8f" % spike_profile.avrg())
 
+"""
 
-# for max_tau in [0.0, 0.1, 1.0, 10.0]:
-
-#     sync_profile = spk.spike_sync_profile(spike_trains, max_tau=max_tau)
-#     x, y = sync_profile.get_plottable_data()
-#     plt.figure()
-#     plt.plot(x, y)
-#     plt.ylabel('Synchrony Profile for max_tau = ' + str(max_tau))
-#     plt.xlabel('Time (ms)')
-#     plt.title('Synchrony')
-#     print("Synchrony: %.8f" % sync_profile.avrg())
 
     
      

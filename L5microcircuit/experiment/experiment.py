e3e03b9cba1d6908a4bcce6198e90c38f1e8501f
graham <joe.w.graham@gmail.com>
2020-02-25 01:20:44 -0500

    Now calling h.run() from Python, plotting only one trace

--- L5microcircuit/experiment/experiment.py
+++ L5microcircuit/experiment/experiment.py
@@ -8,6 +8,11 @@ plt.ion()
 
 h.xopen('experiment.hoc')
 
+h.run()
+print('==========================================')
+print(' h.run() completed, back to experiment.py')
+print('==========================================')
+
 time = np.array(h.PCt[0].to_python())
 
 pyr_soma_traces = []
@@ -37,17 +42,19 @@ for inh_trace in h.INv:
     inh_traces.append(np.array(inh_trace.to_python()))
 
 for pyr_index, pyr_soma_trace in enumerate(pyr_soma_traces):
+
+    if pyr_index == 0:
     
-    plt.figure()
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Membrane Potential (mV)')
-    plt.title('Pyramidal cell ' + str(pyr_index))
-    plt.plot(time, pyr_soma_trace, label='soma')
-    #plt.plot(time, pyr_axon_traces[pyr_index], label='axon')
-    plt.plot(time, pyr_basal_traces[pyr_index], label='basal')
-    #plt.plot(time, pyr_proxap_traces[pyr_index], label='prox ap')
-    #plt.plot(time, pyr_distap_traces[pyr_index], label='dist ap')
-    plt.legend()
+        plt.figure()
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Membrane Potential (mV)')
+        plt.title('Pyramidal cell ' + str(pyr_index))
+        plt.plot(time, pyr_soma_trace, label='soma')
+        #plt.plot(time, pyr_axon_traces[pyr_index], label='axon')
+        plt.plot(time, pyr_basal_traces[pyr_index], label='basal')
+        #plt.plot(time, pyr_proxap_traces[pyr_index], label='prox ap')
+        #plt.plot(time, pyr_distap_traces[pyr_index], label='dist ap')
+        plt.legend()
 
 """
 for inh_index, inh_trace in enumerate(inh_traces):
